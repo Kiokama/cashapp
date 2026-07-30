9df5a3ff71352d1010aa877129f3409edbb2aebd
Kiokama <khoavl456123@gmail.com>
2026-07-30 23:41:43 +0700

    refactor: remove unused project configuration files

--- server-springboot/src/main/java/com/cashapp/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cashapp.repository;
-
-import com.cashapp.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    Optional<User> findByEmailIgnoreCase(String email);
-}
